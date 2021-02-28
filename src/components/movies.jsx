626ffee83fa66d7206d0ab5b16212e19d159c33f
venkata sandeep kumar karamsetty <sandeepsandy2530@GMAIL.COM>
2021-02-28 17:28:57 +1100

    Added rendering Like selection for movies

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,16 +1,24 @@
 
 import React, { Component } from 'react';
 import { getMovies } from '../services/fakeMovieService';
-
+import Like from './common/like';
 class Movies extends Component {
     state = { 
         movies: getMovies()
      }
 
-     handleDelete = (movie) => {
+     handleDelete = (movie) => { 
          const movies = this.state.movies.filter(m => m._id !== movie._id);
          this.setState({ movies });
      };
+
+     handleLike = (movie) => {  // logic for rendering and updating things on screen of liked section
+         const movies = [...this.state.movies];
+         const index = movies.indexOf(movie);
+         movies[index] = {...movies[index]};
+         movies[index].liked = !movies[index].liked;
+         this.setState({ movies });
+     }
 
 
     render() { 
@@ -28,6 +36,7 @@ class Movies extends Component {
                     <th>Genre</th>
                     <th>Stock</th>
                     <th>Rate</th>
+                    <th>Like</th>
                     <th></th>
                 </tr>
             </thead>
@@ -38,6 +47,7 @@ class Movies extends Component {
                         <td>{movie.genre.name}</td>
                         <td>{movie.numberInStock}</td>
                         <td>{movie.dailyRentalRate}</td>
+                        <td><Like liked={movie.liked} onClick={() => this.handleLike(movie)}/></td>
                         <td><button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">Delete</button></td>
                     </tr>
                 ))}
