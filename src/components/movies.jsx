56c30e933f620f910a5b9174507eae390ef31219
venkata sandeep kumar karamsetty <sandeepsandy2530@GMAIL.COM>
2021-03-03 16:36:58 +1100

    Added Pagination for json table display

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -2,9 +2,14 @@
 import React, { Component } from 'react';
 import { getMovies } from '../services/fakeMovieService';
 import Like from './common/like';
+import Pagination from './common/pagination';
+import { paginate } from "../utils/paginate";
+
 class Movies extends Component {
     state = { 
-        movies: getMovies()
+        movies: getMovies(),
+        currentPage: 1,
+        pageSize: 4
      }
 
      handleDelete = (movie) => { 
@@ -20,12 +25,19 @@ class Movies extends Component {
          this.setState({ movies });
      }
 
+     handlePageChange = page => {
+         this.setState({ currentPage: page});
+     }
+
 
     render() { 
         const { length: count} = this.state.movies;
+        const {pageSize, currentPage, movies: allMovies} = this.state;
 
         if(this.state.movies.length === 0)
             return <p>There are no movies in the dataBase.</p>;
+        
+        const movies = paginate(allMovies, currentPage, pageSize);
 
         return (
         <React.Fragment>
@@ -41,7 +53,7 @@ class Movies extends Component {
                 </tr>
             </thead>
             <tbody>
-                {this.state.movies.map(movie => (
+                {movies.map(movie => (
                     <tr key={movie._id}>
                         <td>{movie.title}</td>
                         <td>{movie.genre.name}</td>
@@ -53,8 +65,14 @@ class Movies extends Component {
                 ))}
             </tbody>
         </table>
+        <Pagination 
+        itemsCount={count} 
+        pageSize={pageSize} 
+        currentPage={currentPage}
+        onPageChange={this.handlePageChange} 
+        />
         </React.Fragment>
-        )
+        );
     }
 }
  
