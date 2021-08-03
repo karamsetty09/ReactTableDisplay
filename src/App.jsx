b6945e8af8875cdaf65dfcd6046c1d236c7009d4
venkata sandeep kumar karamsetty <venkatasandeepkumarkaramsetty@gmail.com>
2021-08-03 12:28:42 +1000

    Deleted unecessary code. : Start adding functionalities.

--- src/App.jsx
+++ src/App.jsx
@@ -7,31 +7,11 @@ import Another from './components/todoappUI/Another';
 import Todo from './components/todoappUI/todo';
 
 function App() {
-
-  const[count, setCount] = useState(0);
-
-  function increment(){
-    setCount(prevCount => prevCount+1);
-  }
-
-  function decrement(){
-    setCount(prevCount => prevCount - 1);
-  }
-
-
-
-  const somestyle={
-    background: '#007bff',
-    color: 'white',
-    fontSize: '28px',
-    fontWeight: 'bold',
-  }
   return (
     <main className="container">
       <Another name="Venkata" />
-      {/* <Movies /> */}
       <Todo /> 
-      
+      <Movies />
     </main>
   );
 }
