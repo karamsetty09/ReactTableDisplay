1a63cf3d1dba489b7537d95200a20e6b987a5f9b
venkata <sandeepsandy2530@gmail.com>
2022-04-14 16:13:00 +1000

    store to handle in-memory by using redux for react.

--- src/components/CounterApp/counter.jsx
+++ src/components/CounterApp/counter.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import Hr from '../common/hr';
 import { Header } from '../header/header';
+import {useDispatch, useSelector} from 'react-redux';
+import './counter.scss';
+import {decrement, increment, set} from './actions';
 
 const CounterApp = () => {
     const center = {
         textAlign: 'center',
       };
+    const incident = 'Incident';
+    const count = useSelector((state) => state.count);
+    const dispatch = useDispatch();
+
   return (
     <div>
         <hr/>
@@ -13,6 +20,16 @@ const CounterApp = () => {
         <Header text="Counter-App" color="white"/>
         </div>
         <Hr/>
+        <main className="Counter">
+          <h1>Days Since Last {incident}</h1>
+          <p className="count">{count}</p>
+          <section className="controls">
+            <button onClick={()=>dispatch(increment())}>Increment</button>
+            <button onClick={()=>dispatch(set(0))}>Reset</button>
+            <button onClick={()=>dispatch(decrement())}>Decrement</button>
+          </section>
+    </main>
+  );
     </div>
   )
 }
