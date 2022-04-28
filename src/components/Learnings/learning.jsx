781943913b2095793a19d95eec4edff21df561be
venkata <sandeepsandy2530@gmail.com>
2022-04-28 22:58:13 +1000

    concept of composition in react.

--- src/components/Learnings/learning.jsx
+++ src/components/Learnings/learning.jsx
@@ -1,6 +1,8 @@
 import React, { Fragment } from 'react';
 import Hr from '../common/hr';
 import { Header } from '../header/header';
+import Card from './Card';
+import './Learning.css'
 
 import ExpenseItem from './ExpenseItem';
 import {createStore, compose, applyMiddleware, bindActionCreators, combineReducers} from "redux";
@@ -90,19 +92,21 @@ function Learning() {
       <Header text="Learnings" color="white"/>
       </div>
       <Hr/>
-
+    <Card className='expenses'>
       <ExpenseItem title={expenses[0].title} 
-      amount={expenses[0].amount} 
-      date={expenses[0].date}/>
-      <ExpenseItem title={expenses[1].title} 
-      amount={expenses[1].amount} 
-      date={expenses[1].date}/>
-      <ExpenseItem title={expenses[2].title} 
-      amount={expenses[2].amount} 
-      date={expenses[2].date}/>
-      <ExpenseItem title={expenses[3].title} 
-      amount={expenses[3].amount} 
-      date={expenses[3].date}/>
+        amount={expenses[0].amount} 
+        date={expenses[0].date}/>
+        <ExpenseItem title={expenses[1].title} 
+        amount={expenses[1].amount} 
+        date={expenses[1].date}/>
+        <ExpenseItem title={expenses[2].title} 
+        amount={expenses[2].amount} 
+        date={expenses[2].date}/>
+        <ExpenseItem title={expenses[3].title} 
+        amount={expenses[3].amount} 
+        date={expenses[3].date}/>
+    </Card>
+      
     </div>
   )
 }
